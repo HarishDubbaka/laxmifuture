4efedf78d669948b76267b9b253094bbc0833649
DUBBAKA HARISH <166729357+HarishDubbaka@users.noreply.github.com>
2025-07-20 09:15:10 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-echo "Installing dependencies..."
-yum install -y wget unzip java-1.8.0-openjdk
+sudo yum install tomcat -y
+sudo yum -y install httpd
+sudo cat << EOF > /etc/httpd/conf.d/tomcat_manager.conf
+<VirtualHost *:80>
+  ServerAdmin root@localhost
+  ServerName app.nextwork.com
+  DefaultType text/html
+  ProxyRequests off
+  ProxyPreserveHost On
+  ProxyPass / http://localhost:8080/nextwork-web-project/
+  ProxyPassReverse / http://localhost:8080/nextwork-web-project/
+</VirtualHost>
+EOF
 
-TOMCAT_VERSION=9.0.87
-echo "Downloading Apache Tomcat $TOMCAT_VERSION..."
-wget https://downloads.apache.org/tomcat/tomcat-9/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz -O tomcat.tar.gz
-
-mkdir -p /opt/tomcat
-tar -xvzf tomcat.tar.gz -C /opt/tomcat --strip-components=1
-chmod +x /opt/tomcat/bin/*.sh
